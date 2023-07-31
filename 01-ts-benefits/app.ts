48cb96f0d0183548418d72d464e730e4d494317c
lulla-by <thfl3363@hanmail.net>
2023-07-31 17:42:56 +0900

    숫자 문자열 및 불리언 작업하기

--- 01-ts-benefits/app.ts
+++ 01-ts-benefits/app.ts
@@ -1,12 +1,18 @@
-function add(n1: number, n2: number) {
-  if(typeof n1 !== 'number' || typeof n2 !=='number'){
-    throw new Error("Incorrect input!")
+function add(n1: number, n2: number, showResult:boolean,pharse:string) {
+  // if(typeof n1 !== 'number' || typeof n2 !=='number'){
+  //   throw new Error("Incorrect input!")
+  // }
+  const result = n1 + n2
+  if(showResult){
+    console.log(pharse + result)
+  }else{
+    return result;
   }
-  return n1 + n2;
 }
 
-const number1 = 5;
+const number1 = 5; //5.0 본질적으로 같은 숫자
 const number2 = 2.8;
+const printResult = true;
+const resultPharse = 'Resulte is: ';
 
-const result = add(number1, number2);
-console.log(result);
+const result = add(number1, number2,printResult,resultPharse);

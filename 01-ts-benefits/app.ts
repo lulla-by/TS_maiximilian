a54069b9551f915944c367a4f44af0d45ac3918b
lulla-by <thfl3363@hanmail.net>
2023-07-31 18:08:23 +0900

    객체형태 - 명시적 타입 지정 - 타입 추론

--- 01-ts-benefits/app.ts
+++ 01-ts-benefits/app.ts
@@ -1,22 +1,14 @@
-function add(n1: number, n2: number, showResult:boolean,pharse:string) {
-  // if(typeof n1 !== 'number' || typeof n2 !=='number'){
-  //   throw new Error("Incorrect input!")
-  // }
-  const result = n1 + n2
-  if(showResult){
-    console.log(pharse + result)
-  }else{
-    return result;
-  }
+//명시적 타입지정
+// const person:{
+//   name:string;
+//   age:number;
+// } = {
+  const person ={
+  name:"Maximilian",
+  age:30
 }
 
-let number1:number; 
-number1 = 5;//5.0 본질적으로 같은 숫자
-const number2 = 2.8;
-const printResult = true;
-// TS는 string으로 추론하는데
-let resultPharse = 'Resulte is: ';
-// 숫자로 재할당하려고 시도하면 에러 발생
-// resultPharse = 0;
+// console.log(person.nickname);
+// 존재하지 않는 프로퍼티에 접근시 에러
 
-const result = add(number1, number2,printResult,resultPharse);
+console.log(person.name)

bb604f182e760487e2509329a05c85940b63eb58
lulla-by <thfl3363@hanmail.net>
2023-08-01 14:09:24 +0900

    튜플 작업하기

--- 01-ts-benefits/app.ts
+++ 01-ts-benefits/app.ts
@@ -3,11 +3,34 @@
 //   name:string;
 //   age:number;
 // } = {
-  const person ={
+  const person:{
+    name:string;
+    age:number;
+    hobbies:string [];
+    role:[number,string]
+  } ={
   name:"Maximilian",
   age:30,
-  hobbies:["Sports","Cooking"]
+  hobbies:["Sports","Cooking"],
+// TS에는 바닐라 자바스크립트에서는 없던 몇가지 새로운 개념과 타입이 추가
+// 그 중 하나가 바로 Tuple
+// 배열은 맞지만 길이와 타입이 고정된 배열
+role:[2,"author"]
+
 }
+
+// push는 예외적으로 튜플에서 허용되기에 
+// 안타깝게도 TS가 이런 에러를 걸러내질 못하지만 적어도 잘못된 값을 할당하지는 않음 
+// person.role.push("admin")
+// for(const role of person.role){
+//   console.log(role) // SPORTS COOKING
+// }
+
+// person.role[1]=10
+
+// 길이는 강제되지 않지만 푸시 등에는 해당되지 않음
+//Type '[number, string, string]' is not assignable to type '[number, string]'.
+// person.role=[0,"1","2"]
 
 let favoriteActivities:string[];
 favoriteActivities=["Sprots"]
@@ -27,3 +50,4 @@ for(const hobby of person.hobbies){
   // console.log(hobby.map());
   
 }
+

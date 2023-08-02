bd4ca8c5fbdefcdbedc0d14edbc98cc89508640a
lulla-by <thfl3363@hanmail.net>
2023-08-02 18:47:53 +0900

    public, private를 사용한 접근제어

--- 03.class,interface/src/app.ts
+++ 03.class,interface/src/app.ts
@@ -1,5 +1,8 @@
 class Department {
-  name: string;
+  // private 사용: class 즉, 생성된 객체 내부에서만 접근가능
+  // 기본은 public
+  private name: string;
+  private employees:string[] = []
 
   constructor(name: string) {
     this.name = name;
@@ -8,15 +11,31 @@ class Department {
   describe(this:Department) {
     console.log(this.name);
   }
+  addEmployee(employee:string){
+    this.employees.push(employee)
+  }
+  printEmployeeInformation(){
+    console.log(this.employees.length)
+    console.log(this.employees)
+  }
 }
 
 const accounting = new Department("Accounting");
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Jay");
+
+// 클래스를 사용하는 방법은 확실한 한 가지로 정하고 다른 방법은 막아두는 것이 좋음
+// 클래스 외부에서 employees에 접근하는 것을 허용해서는 X 
+// accounting.employees[2]="Anna";
+
+accounting.printEmployeeInformation()
 accounting.describe()
 
-const accountingCopy = {name:"Dummy",describe:accounting.describe }
 
-// 이 경우에 this는 Department 타입의 객체를 참조하지 않음 
-accountingCopy.describe() // undefined
-// 이 객체에  describeCopy 속성을 추가했기 때문, 이 클래스나 정의된 특정 클래스를 기반으로 하지 않고 더미 객체로서 생성되는 것
-console.log(accountingCopy); 
+
+
+// const accountingCopy = {name:"Dummy",describe:accounting.describe }
+
+// accountingCopy.describe();
 

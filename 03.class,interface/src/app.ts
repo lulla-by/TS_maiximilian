05729b243015212a3a444f0baadb602928e1d98c
lulla-by <thfl3363@hanmail.net>
2023-08-04 14:29:40 +0900

    상속 - super() 키워드

--- 03.class,interface/src/app.ts
+++ 03.class,interface/src/app.ts
@@ -1,47 +1,60 @@
 class Department {
   // private id : string;
   // private name: string;
-  private employees:string[] = []
+  private employees: string[] = [];
 
   // 필드를 찾은 다음 값을 저장해야 하는 이중 초기화 코드를 한 번에 처리하도록 축약한 것
-  constructor(private readonly id:string, private name: string) {
-    //readonly: 특정 속성이 초기화되고나면 이후에는 변경되어서는 안된다는 점을 명확히 하기 위해 몇 ㅏ지 추가적이 ㄴ안전성을 더해줌
-    // this.id = id;
-    // this.name = name;
-  }
+  constructor(private readonly id: string, private name: string) {}
 
-  describe(this:Department) {
+  describe(this: Department) {
     console.log(`Department (${this.id}): ${this.name}`);
   }
-  addEmployee(employee:string){
-
+  addEmployee(employee: string) {
     //Cannot assign to 'id' because it is a read-only property.
-    this.id = 3;
+    // this.id = 3;
 
-    this.employees.push(employee)
+    this.employees.push(employee);
   }
-  printEmployeeInformation(){
-    console.log(this.employees.length)
-    console.log(this.employees)
+  printEmployeeInformation() {
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("d1","Accounting");
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    // 상속하는 클래스로 super를 추가하고 이를 함수처럼 실행해야 함
+    // super는 기본 클래스의 생성자를 호출함
+    super(id, "IT");
+    // 특수한 속성을 할당하려면 super부터 호출해야 함
+    this.admins = admins;
+  }
+}
 
-accounting.addEmployee("Max");
-accounting.addEmployee("Jay");
+class AccountingDepartment extends Department {
 
-// 클래스를 사용하는 방법은 확실한 한 가지로 정하고 다른 방법은 막아두는 것이 좋음
-// 클래스 외부에서 employees에 접근하는 것을 허용해서는 X 
-// accounting.employees[2]="Anna";
+  constructor(id: string,private reports:string[]) {
+    super(id, "Accuonting");
+  }
 
-accounting.printEmployeeInformation()
-accounting.describe()
+  addReport(text:string){
+    this.reports.push(text)
+  }
 
+  printReport(){
+    console.log(this.reports);
+    
+  }
+}
 
+const accounting = new AccountingDepartment("d2",[])
+accounting.addReport("Something wet wrong")
+accounting.printReport()
 
+const it = new ITDepartment("d1", ["max"]);
+it.addEmployee("Jay");
+it.addEmployee("henry");
 
-// const accountingCopy = {name:"Dummy",describe:accounting.describe }
-
-// accountingCopy.describe();
-
+it.printEmployeeInformation();
+it.describe();
+console.log(it);

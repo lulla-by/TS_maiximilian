5c828cb107b2091ac0a34bd30a22e093c3b914c2
lulla-by <thfl3363@hanmail.net>
2023-08-04 14:09:09 +0900

    readonly 속성

--- 03.class,interface/src/app.ts
+++ 03.class,interface/src/app.ts
@@ -4,7 +4,8 @@ class Department {
   private employees:string[] = []
 
   // 필드를 찾은 다음 값을 저장해야 하는 이중 초기화 코드를 한 번에 처리하도록 축약한 것
-  constructor(private id:string, private name: string) {
+  constructor(private readonly id:string, private name: string) {
+    //readonly: 특정 속성이 초기화되고나면 이후에는 변경되어서는 안된다는 점을 명확히 하기 위해 몇 ㅏ지 추가적이 ㄴ안전성을 더해줌
     // this.id = id;
     // this.name = name;
   }
@@ -13,6 +14,10 @@ class Department {
     console.log(`Department (${this.id}): ${this.name}`);
   }
   addEmployee(employee:string){
+
+    //Cannot assign to 'id' because it is a read-only property.
+    this.id = 3;
+
     this.employees.push(employee)
   }
   printEmployeeInformation(){

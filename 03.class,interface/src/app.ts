bb77846cdcc9507215faf459f6d54a45d6071b14
lulla-by <thfl3363@hanmail.net>
2023-08-05 16:40:43 +0900

    선택적 매개변수 & 속성

--- 03.class,interface/src/app.ts
+++ 03.class,interface/src/app.ts
@@ -1,11 +1,4 @@
-
-// type지정
-// type AddFn = (a: number, b: number) => number;
-
-// interface 지정
 interface AddFn {
-  // TS는 인터페이스의 이 익명 함수의 특수한 구문을 이해하며 
-  // 이 인터페이스를 함수 타입으로서 사용하고자 하는 경우 함수의 형태가 이와 같다는 것을 이해함
   (a:number,b:number):number;
 }
 
@@ -14,29 +7,38 @@ add = (a, b) => a + b;
 add(6, 4);
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?:string
 }
 
 interface Greetable extends Named {
   greet(phrase: string): void;
+  
 }
 
 class Person implements Greetable {
-  name: string;
-  age: number;
+  name?: string;
+  age = 30;
 
-  constructor(name: string, age: number) {
+  constructor(name?: string) {
     this.name = name;
-    this.age = age;
+    if(name){
+      this.name = name;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + " " + this.name);
+    if(this.name){ 
+      console.log(phrase + " " + this.name);
+    }else{
+      console.log("Hi!");
+      
+    }
   }
 }
 
 let user1: Greetable;
-user1 = new Person("max", 24);
+user1 = new Person();
 
 user1.greet("Hi there. i'm");
 console.log(user1);

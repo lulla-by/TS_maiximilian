10da540cae942f7e291c8e32676baf038864bd14
lulla-by <thfl3363@hanmail.net>
2023-08-08 16:44:02 +0900

    다른 일반 함수

--- 05.generics/src/app.ts
+++ 05.generics/src/app.ts
@@ -18,11 +18,31 @@ const names: Array<string> = [];
 
 //Object.assign() 메서드는 출처 객체들의 모든 열거 가능한 자체 속성을 복사해 대상 객체에 붙여넣고 그 후 대상 객체를 반환
 //제네릭 타입 다음에 있는 홑화살괄호 내에 extends를 입력 및 특정 제약 조건을 설정
-function merge<T extends object, U extends object > (objA: T, objB: U){
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
 // const mergedObj = merge({name:"max"},{age:30}) as {name:string,age:number}
-const mergedObj = merge({ name: "max",hobbies:["sports"] },{age:30});
+const mergedObj = merge({ name: "max", hobbies: ["sports"] }, { age: 30 });
 console.log(mergedObj);
-console.log(mergedObj.name);
+// console.log(mergedObj.name);
+
+interface Lengthy {
+  // 그저 length 속성이 있는지만 확인
+  length:number;
+}
+
+// 제약 조건을 사용하요 얻는 것이 무엇이든 length 속성도 반환되며 배열이나 문자열은 length 속성을 지닌다는 것
+function countAndDescribe<T extends Lengthy>(element: T):[T,string] {
+
+  let descriptionText = "Got no  value";
+if(element.length === 1){
+  descriptionText = "Got 1 elements."
+}else if(element.length > 1){
+  descriptionText = "Got " + element.length + " elements."
+}
+  return [element, descriptionText]
+}
+
+console.log(countAndDescribe("Hi There!"));
+console.log(countAndDescribe(["Sport!","Cooking"]));

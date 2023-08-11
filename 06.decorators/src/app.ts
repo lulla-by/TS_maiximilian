3f2d5c287813e8f2a335d1d246b5492b43a7e589
lulla-by <thfl3363@hanmail.net>
2023-08-11 13:07:50 +0900

    클래스 데코레이터에서의 새로운 클래스 반환

--- 06.decorators/src/app.ts
+++ 06.decorators/src/app.ts
@@ -1,42 +1,48 @@
-// function Logger(logString: string) {
-//   console.log("로거팩토리"); //1
-//   return function (constructor: Function) {
-//     console.log(logString); //6
-//     console.log(constructor); //7
-//   };
-// }
+function Logger(logString: string) {
+  console.log("로거팩토리");
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
 
-// function WithTemplate(template: string, hookId: string) {
-//   console.log("탬플릿팩토리"); //2
-//   return function (constructor: any) {
-//     console.log("탬플릿 데코레이터 함수 실행"); //3
-//     console.log(constructor); //4
-//     const hookEl = document.getElementById(hookId);
-//     const p = new constructor();
-//     if (hookEl) {
-//       hookEl.innerHTML = template;
-//       hookEl.querySelector("h1")!.textContent = p.name;
-//     }
-//   };
-// }
+function WithTemplate(template: string, hookId: string) {
+  console.log("탬플릿팩토리");
+  return function<T extends {new(...args:any[]):{name:string}}> (originalConstructor: T) {
+    // 기존의 Person 클래스를 확장시킴(originaConstructor === Person)
+    // 데코레이터를 더한 새로운 클래스로 대체, constructor함수의 syntactic sugar
+    return class extends originalConstructor {
+      constructor(..._:any[]) {
+        super();
+        console.log("탬플릿 데코레이터 함수 실행");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
 
-// // 우리가 팩토리 함수를 명시한 순서 내에서 실 데코레이터가 나타남
-// // 하지만 실 데코레이터 함수의 실행은 밑에서부터 일어남
+// 우리가 팩토리 함수를 명시한 순서 내에서 실 데코레이터가 나타남
+// 하지만 실 데코레이터 함수의 실행은 밑에서부터 일어남
 
-// // @Logger("LOGGING - PERSON")
-// @Logger("로거 데코레이터 함수 실행")
-// @WithTemplate("<h1>My Person Object</h1>", "app")
-// class Person {
-//   name = "Max";
+// @Logger("LOGGING - PERSON")
+@Logger("로거 데코레이터 함수 실행")
+//  WithTemplate(template: string, hookId: string)
+@WithTemplate("<h1>My Person Object</h1>", "app")
+class Person {
+  name = "Max";
 
-//   constructor() {
-//     console.log("Creating person object..."); //5,8
-//   }
-// }
+  constructor() {
+    console.log("Creating person object..."); //5,8
+  }
+}
 
-// const pers = new Person();
+const pers = new Person();
 
-// console.log(pers); //7
+console.log(pers); //7
 
 // ---------
 
@@ -48,34 +54,37 @@ function Log(target: any, propertyName: string | Symbol) {
 
 function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
   console.log("Log 2 Accessor decorator!");
-  console.log(target);  //prototype
-  console.log(name);  // price
+  console.log(target); //prototype
+  console.log(name); // price
   console.log(descriptor); //property descriptor set 함수
   console.log("----------------------------");
 }
 
-function Log3(target: any, name:string | Symbol, descriptor:PropertyDescriptor){
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
   console.log("Log 3 Method decorator!");
   console.log(target);
   console.log(name);
-  console.log(descriptor); 
+  console.log(descriptor);
   console.log("----------------------------");
 }
-function Log4(target: any, name:string | Symbol, position:number){
+function Log4(target: any, name: string | Symbol, position: number) {
   console.log("Log 4 parameter decorator!");
   console.log(target);
-  console.log(name);  // 이 매개변수를 사용하는 메서드의 이름 출력
-  console.log(position);  // 인수의 인덱스
+  console.log(name); // 이 매개변수를 사용하는 메서드의 이름 출력
+  console.log(position); // 인수의 인덱스
   console.log("----------------------------");
-  
 }
 
 class Product {
-  @Log
+  // @Log
   title: string;
   private _price: number;
 
-  @Log2
+  // @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -89,9 +98,12 @@ class Product {
     this._price = p;
   }
 
-  @Log3
-  getPriceWithTax(@Log4 tax: number) {
+  // @Log3
+  // getPriceWithTax(@Log4 tax: number) {
+  getPriceWithTax(tax: number) {
     return this._price * (1 + tax);
   }
 }
 
+let book1 = new Product("book1", 19);
+let book2 = new Product("book2", 29);

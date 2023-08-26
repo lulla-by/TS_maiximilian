112fbd6f052ea8729d70cd509c0ce9ca676c25cf
lulla-by <thfl3363@hanmail.net>
2023-08-26 18:36:44 +0900

    React와 Typescript의 작동

--- 07.React_Typescript/src/App.tsx
+++ 07.React_Typescript/src/App.tsx
@@ -1,9 +1,13 @@
+import React from "react";
 
+//FC: Function Component의 약자, 최근에는 사용하지 않는 추세(다른 부작용들 때문에), 대신 기본적인 fucntion사용
+const App :React.FC = () => {
 
-function App() {
+  const a:string = "string"
+
   return (
     <div className="App">
-   <h1>hi</h1>
+      <h1>{a}</h1>
     </div>
   );
 }

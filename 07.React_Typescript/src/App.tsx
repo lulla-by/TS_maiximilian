53c5854dfd812fede6488d0f9f561eec4d9c3dad
lulla-by <thfl3363@hanmail.net>
2023-08-26 19:44:11 +0900

    ref 로 사용자 입력 받기, cross-component 커뮤니케이션

--- 07.React_Typescript/src/App.tsx
+++ 07.React_Typescript/src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import TodoList from "./components/TodoList";
+import NewTodo from './components/NewTodo';
 
 //FC: Function Component의 약자, 최근에는 사용하지 않는 추세(다른 부작용들 때문에), 대신 기본적인 fucntion사용
 const App :React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the course" }];
+  const todoAddHandler = (text:string) =>{
+    console.log(text)
+  }
   return (
     <div className="App">
       {/* A component that adds todo */}
-      <TodoList items = {todos}/>
+      <NewTodo onAddTodo={todoAddHandler}/>
+      <TodoList items = {todos} />
     </div>
   );
 }

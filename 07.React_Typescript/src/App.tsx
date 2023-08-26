5ee0473ee5be74a3547a268897983ee3e66db82c
lulla-by <thfl3363@hanmail.net>
2023-08-26 19:53:30 +0900

    useState를 통한 상태 밑 타입 관리 - useState의 경우 어떤값으로 이루어져있는지 타입을 지정해줘야함 - interface나 type을 사용 - todo.models.ts파일을 새로 생성하여 인터페이스 지정

--- 07.React_Typescript/src/App.tsx
+++ 07.React_Typescript/src/App.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from './components/NewTodo';
+import {Todo} from './todo.models'
 
 //FC: Function Component의 약자, 최근에는 사용하지 않는 추세(다른 부작용들 때문에), 대신 기본적인 fucntion사용
 const App :React.FC = () => {
-  const todos = [{ id: "t1", text: "Finish the course" }];
+  // 이렇게 빈 배열로 state를 설정하면 TS는 이게 영원히 빈배열로 이뤄진다고 생각 => 무언가를 집어넣으면 에러 발생
+  // const [todos,setTodos] = useState([]);
+
+  // 이 state는 type이 string인 id와 type이 string인 text로 이루어진 객체의 배열이라고 알려줘야 함
+  // todo.models.tes라는 파일 생성하여 interface 설정
+  const [todos,setTodos] = useState<Todo[]>([]);
+
   const todoAddHandler = (text:string) =>{
-    console.log(text)
+    setTodos([{id:Math.random().toString(),text}])
+
+    console.log(todos);
+    
   }
   return (
     <div className="App">
